2d5dbc7c64061f794934ab865308428e97ceb1f9
hongxeob <sjun2918@naver.com>
2022-09-19 22:59:15 +0900

    글목록 보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,17 +1,25 @@
 package com.cos.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+
 	@GetMapping({ "", "/" })
-	public String index() {
-		// /WEB-INF/views/index.jsp
-		return "index";
+	public String index(Model model) {
+		model.addAttribute("boards", boardService.글목록());
+
+		return "index"; //viewResolver가 작동! 앞뒤로 프리픽스/서픽스를 붙혀준다.
 	}
-	
+
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
 		return "board/saveForm";
