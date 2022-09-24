ed555f8031df678d47ec979d5eefdf5bc76a1c53
hongxeob <sjun2918@naver.com>
2022-09-24 21:34:17 +0900

    회원정보 수정2

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,14 +1,22 @@
 package com.cos.blog.controller.api;
 
 import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
@@ -19,16 +27,29 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
-		System.out.println("UserApiController : save 호출됨.");		
+		System.out.println("UserApiController : save 호출됨.");
 		userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
+
 	@PutMapping("/user")
-	public ResponseDto<Integer> update(@RequestBody User user){
+	public ResponseDto<Integer> update(@RequestBody User user) { // key=value, x-www-form-urlencoded
 		userService.회원수정(user);
-		return new ResponseDto<Integer> (HttpStatus.OK.value(),1);
+		// 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 됐음.
+		// 하지만 세션값은 변경되지 않은 상태이기 때문에 우리가 직접 세션값을 변경해줄 것임.
+		// 세션 등록
+
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
- 
+
+
+
 }
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -3,6 +3,10 @@ package com.cos.blog.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +23,7 @@ public class UserService {
 	
 	@Autowired
 	private BCryptPasswordEncoder encoder;
+	
 
 	@Transactional // 이 밑의 코드들이 하나의 트랜잭션이 되어서 성공을 하면 하나의 커밋이 됨.
 	public void 회원가입(User user) {
@@ -32,9 +37,9 @@ public class UserService {
 
 	@Transactional
 	public void 회원수정(User user) {
-		//수정시에는 영속성 컨텍스트 User 오브젝트를 영속화 시키고, 영속화된 User 오브젝트를 수정.
-		//select를 해서 User 오브젝트를 DB로부터 가져오는 이뉴는 영속화를 하기 위해서.
-		//영속화된 오브젝트를 변경하면 자동으로 DB에 update문을 날려준다.
+		// 수정시에는 영속성 컨텍스트 User 오브젝트를 영속화시키고, 영속화된 User 오브젝트를 수정
+		// select를 해서 User오브젝트를 DB로 부터 가져오는 이유는 영속화를 하기 위해서
+		// 영속화된 오브젝트를 변경하면 자동으로 DB에 update문을 날려줌.
 		User persistance = userRepository.findById(user.getId()).orElseThrow(()->{
 			return new IllegalArgumentException("회원 찾기 실패");
 		});
@@ -42,8 +47,8 @@ public class UserService {
 		String encPassword = encoder.encode(rawPassword);
 		persistance.setPassword(encPassword);
 		persistance.setEmail(user.getEmail());
-		//회원수정 함수 종료시 = 서비스 종료=트랜잭션 종료=commit 자동으로 됨.
-		//커밋이 자동으로 된다는건 영속화된 퍼시스턴스 객채의 변화가 감지되면 더티체킹이 되어 update문을 날려줌.
-		
+
+
+
 	}
 }
